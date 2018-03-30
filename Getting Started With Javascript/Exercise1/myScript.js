36cf44c1329f13a2627fb9e2747bb72213180e9d
willksullivan <36898585+willksullivan@users.noreply.github.com>
2018-03-29 20:08:42 -0500

    push

--- Getting Started With Javascript/Exercise1/myScript.js
+++ Getting Started With Javascript/Exercise1/myScript.js
@@ -1,1 +1,11 @@
-alert("Hello World");
+//this is a message
+console.log("This is a test");
+
+//this is an error
+console.error("this is an error");
+
+//this is an info 
+console.info("THis is info");
+
+//output a warning
+console.warn("Now we have a warning");
